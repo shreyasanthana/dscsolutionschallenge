d6c48cf1b4758e6b749afb9c90e4147312a4173f
ninaadlakshman <ninaadlak@gmail.com>
2022-03-08 22:20:12 -0500

    Updates to Popup HTML and Feature
    Included new text highlighting feature and updated code in popup.html

--- disability-chrome-extension/src/app/app.component.ts
+++ disability-chrome-extension/src/app/app.component.ts
@@ -1,37 +1,58 @@
 /// <reference types="chrome"/>
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'disability-chrome-extension';
   focusSelected = false;
+  textHighlightingSelected = false;
   increaseTextButtonSelected = false;
   focusIconColor = "red";
   currentFocusIcon = "pi pi-eye-slash";
+  currentTextHighlightingIcon = "pi pi-eye-slash";
   currentIncreaseTextButtonIcon = "pi pi-eye-slash";
   blurIntensity: number = 3;
 
-  constructor() {
+  ngOnInit() {
+    // chrome.storage.sync.get(['focusSelected'], function(result) {
+    //   this.focusSelected = result;
+    //   this.setFocusIcons();
+    // });
+  }
 
+  setFocusIcons() {
+    if (this.focusSelected) {
+      this.currentFocusIcon = "pi pi-eye";
+      this.focusIconColor = "green";
+    } else {
+      this.currentFocusIcon = "pi pi-eye-slash";
+      this.focusIconColor = "red";
+    }
+  }
+
+  setTextHighlightingIcons() {
+    if (this.textHighlightingSelected) {
+      this.currentTextHighlightingIcon = "pi pi-eye";
+    } else {
+      this.currentTextHighlightingIcon = "pi pi-eye-slash";
+    }
   }
 
   onFocusClick() {
     this.focusSelected = !this.focusSelected;
+    this.setFocusIcons();
+    
+    
+    //chrome.storage.sync.set({'focusSelected': this.focusSelected});
     if (this.focusSelected) {
-      this.currentFocusIcon = "pi pi-eye";
-      this.focusIconColor = "green";
-
       chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
         let message = "enableFocusHoveredArea:" + this.blurIntensity;
         chrome.tabs.sendMessage(tabs[0].id, message);
       });
     } else {
-      this.currentFocusIcon = "pi pi-eye-slash";
-      this.focusIconColor = "red";
-
       chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
         chrome.tabs.sendMessage(tabs[0].id, "disableFocusHoveredArea");
       });
@@ -40,9 +61,26 @@ export class AppComponent {
 
   updateBlurIntensity() {
     chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-      let message = "changeIntensity:" + this.blurIntensity;
+      let message = "updateBlurIntensity:" + this.blurIntensity;
       chrome.tabs.sendMessage(tabs[0].id, message);
     });
+  }
+
+  onTextHighlightingClick() {
+    this.textHighlightingSelected = !this.textHighlightingSelected;
+    this.setTextHighlightingIcons();
+
+    if (this.textHighlightingSelected) {
+      chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+        let message = "enableTextHighlighting";
+        chrome.tabs.sendMessage(tabs[0].id, message);
+      });
+    } else {
+      chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+        let message = "disableTextHighlighting";
+        chrome.tabs.sendMessage(tabs[0].id, message);
+      });
+    }
   }
 
   onIncreaseTextButtonClick() {
--- disability-chrome-extension/src/content.js
+++ disability-chrome-extension/src/content.js
@@ -1,18 +1,29 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.includes("enableFocusHoveredArea")) {
+    if (message.includes("enableFocusHoveredArea") || message.includes("updateBlurIntensity")) {
         var numIndex = message.indexOf(":") + 1;
         var blurIntensity = message.substring(numIndex)
         enableFocusHoveredArea(blurIntensity);
     } else if (message.includes("disableFocusHoveredArea")) {
         disableFocusHoveredArea();
-    } else if (message.includes("changeIntensity")) {
-        var numIndex = message.indexOf(":") + 1;
-        var blurIntensity = message.substring(numIndex)
-        updateFocusHoveredAreaBlur(blurIntensity);
+    } else if (message.includes("enableTextHighlighting")) {
+        enableTextHighlighting();
+    } else if (message.includes("disableTextHighlighting")) {
+        disableTextHighlighting();
     }
 });
 
 var divs = document.getElementsByTagName('div');
+var paragraphs = document.getElementsByTagName('p');
+var h1s = document.getElementsByTagName('h1');
+var h2s = document.getElementsByTagName('h2');
+var h3s = document.getElementsByTagName('h3');
+var h4s = document.getElementsByTagName('h4');
+var h5s = document.getElementsByTagName('h5');
+var h6s = document.getElementsByTagName('h6');
+var lis = document.getElementsByTagName("li");
+var as = document.getElementsByTagName("a");
+
+var textElements = [paragraphs, h1s, h2s, h3s, h4s, h5s, h6s, lis, as];
 
 var largeDivsAndNavs = [];
 
@@ -34,7 +45,6 @@ for (var i = 0; i < largeDivsAndNavs.length; i++) {
 console.log(largeDivsAndNavs);
 
 function enableFocusHoveredArea(blurIntensity) {
-    console.log(blurIntensity)
     for (var i = 0; i < largeDivsAndNavs.length; i++) {
         largeDivsAndNavs[i].addEventListener("mouseover", function(event) {            
             this.style.filter = "blur(0px)";
@@ -58,7 +68,53 @@ function disableFocusHoveredArea() {
     window.location.reload();
 }
 
-function updateFocusHoveredAreaBlur(blurIntensity) {
-    console.log(blurIntensity);
-    enableFocusHoveredArea(blurIntensity);    
+function disableTextHighlighting() {
+    for (var i = 0; i < textElements.length; i++) {
+        for (var element = 0; element < textElements[i].length; element++) {
+            textElements[i][element].removeEventListener("mouseover", function(event) {            
+            }, false);
+            textElements[i][element].removeEventListener("mouseout", function(event) {
+            }, false);
+        }
+    }
+    window.location.reload();
+}
+
+function enableTextHighlighting() {
+    console.log("ENABLE TEXT HIGHLIGHTING")
+    console.log(textElements)
+    for (var i = 0; i < textElements.length; i++) {
+        for (var element = 0; element < textElements[i].length; element++) {
+            var originalBackgroundColor = "transparent";
+            textElements[i][element].addEventListener("mouseover", function() {
+                if (this.style.backgroundColor) {
+                    originalBackgroundColor = this.style.backgroundColor;
+                }
+
+                if (this.style.color && val1ColorGreaterThanVal2Color(this.style.color, "#808080")) {
+                    this.style.backgroundColor = "yellow";
+                } else {
+                    this.style.backgroundColor = "red";
+                }
+            })
+
+            textElements[i][element].addEventListener("mouseout", function() {
+
+                this.style.backgroundColor = originalBackgroundColor;
+            })
+        }
+    }
+}
+
+function val1ColorGreaterThanVal2Color(val1, val2) {
+    console.log("FIRST STYLE COLOR: " + color1);
+    console.log("Hex " + val2);
+    var color1 = parseInt(val1.substring(1), 16);
+    var color2 = parseInt(val2.substring(1), 16);
+
+    if (color1 < color2) {
+        return true;
+    } else {
+        return false;
+    }
 }
