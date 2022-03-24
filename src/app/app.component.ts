8b40b3d5e898a2c4a73cd31c8bbd9ef00328f93a
Aayush Seth <aayushseth8@gmail.com>
2022-03-23 23:39:29 -0700

    Add autoscrolling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,21 @@
 /// <reference types="chrome"/>
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent implements OnInit {
-  title = 'disability-chrome-extension';
+  title = "disability-chrome-extension";
   focusSelected = false;
   textHighlightingSelected = false;
   increaseTextButtonSelected = false;
+  autoScrollingSelected = false;
   focusIconColor = "red";
   currentFocusIcon = "pi pi-eye-slash";
   currentTextHighlightingIcon = "pi pi-eye-slash";
   currentIncreaseTextButtonIcon = "pi pi-eye-slash";
+  currentAutoScrollIcon = "pi pi-eye-slash";
   blurIntensity: number = 3;
   color: string;
 
@@ -51,74 +53,95 @@ export class AppComponent implements OnInit {
     }
   }
 
+  setAutoScrollIcons() {
+    if (this.autoScrollingSelected) {
+      this.currentAutoScrollIcon = "pi pi-eye";
+    } else {
+      this.currentAutoScrollIcon = "pi pi-eye-slash";
+    }
+  }
+
   onFocusClick() {
     this.focusSelected = !this.focusSelected;
     this.setFocusIcons();
-    
-    
+
     //chrome.storage.sync.set({'focusSelected': this.focusSelected});
     if (this.focusSelected) {
-      chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
         let message = "enableFocusHoveredArea:" + this.blurIntensity;
         chrome.tabs.sendMessage(tabs[0].id, message);
       });
     } else {
-      chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
         chrome.tabs.sendMessage(tabs[0].id, "disableFocusHoveredArea");
       });
     }
   }
 
   updateBlurIntensity() {
-    chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       let message = "updateBlurIntensity:" + this.blurIntensity;
       chrome.tabs.sendMessage(tabs[0].id, message);
     });
   }
-
-
 
   onTextHighlightingClick() {
     this.textHighlightingSelected = !this.textHighlightingSelected;
     this.setTextHighlightingIcons();
 
     if (this.textHighlightingSelected) {
-      chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
         let message = "enableTextHighlighting";
         chrome.tabs.sendMessage(tabs[0].id, message);
       });
     } else {
-      chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
         let message = "disableTextHighlighting";
         chrome.tabs.sendMessage(tabs[0].id, message);
       });
     }
   }
 
-
-//step 2
+  //step 2
   onIncreaseTextButtonClick() {
     this.increaseTextButtonSelected = !this.increaseTextButtonSelected;
     this.setIncreaseButtonSizeIcons();
 
     if (this.increaseTextButtonSelected) {
-      chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-        let message = "enableIncreaseTextButton:"  + this.color;
+      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        let message = "enableIncreaseTextButton:" + this.color;
         chrome.tabs.sendMessage(tabs[0].id, message);
       });
     } else {
-      chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
         chrome.tabs.sendMessage(tabs[0].id, "disableIncreaseTextButton");
       });
     }
   }
-//step 3
+  //step 3
   updateButtonColor() {
-    chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       let message = "updateButtonColor:" + this.color;
       chrome.tabs.sendMessage(tabs[0].id, message);
     });
   }
 
-}
+  onAutoScrollButtonClick() {
+    console.log("auto scrolling!");
 
+    this.autoScrollingSelected = !this.autoScrollingSelected;
+    this.setAutoScrollIcons();
+
+    if (this.textHighlightingSelected) {
+      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        let message = "enableAutoScrolling";
+        chrome.tabs.sendMessage(tabs[0].id, message);
+      });
+    } else {
+      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        let message = "disableAutoScrolling";
+        chrome.tabs.sendMessage(tabs[0].id, message);
+      });
+    }
+  }
+}
